5200f44803b8e2fae300c08445572ec2083ea03a
pablopunk <pablo@pablo.im>
2026-09-22 12:49:55 +0200

    fix(voyager): use numeric layer 4 for MO thumb

--- 6JzXq/keymap.c
+++ 6JzXq/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(2, KC_TAB),  KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_RBRC,        
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
     KC_TRANSPARENT, KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_LBRC,        
-                                                    MO(_MOD),    MT(MOD_LSFT, KC_BSPC),                                MT(MOD_LCTL, KC_ENTER),LT(1, KC_SPACE)
+                                                    MO(4),    MT(MOD_LSFT, KC_BSPC),                                MT(MOD_LCTL, KC_ENTER),LT(1, KC_SPACE)
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_DELETE,      
